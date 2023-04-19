7c200683a4e0bc99ccec84136c105e3be9e1ef89
nephydecode <nephydecode@gmail.com>
2023-04-19 23:53:20 +0800

    feat: implement auto ranking logic & auto reload failsafe

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -7,3 +7,20 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     sendResponse("Color message is none.");
   }
 });
+
+if(window.location.href === 'https://www.midjourney.com/app/rank-pairs/'){
+  console.log('In Rank Pairing Page, started auto ranking')
+  setInterval(()=>{
+
+    if(document.getElementsByClassName('flex relative grow justify-center items-center w-full h-full').length!== 0) {
+      (document.getElementsByClassName('flex relative grow justify-center items-center w-full h-full')[getRandomInt(4)].children[0] as HTMLDivElement).click();   
+    } else {
+      location.reload();
+    }
+
+  }, (getRandomInt(15)*1000+1500))
+}
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
